060005aaa33c883701b575fe48722da829678bc0
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2022-11-15 22:32:24 -0800

    Collect data for analysis in Google Colab

--- E96 Final Project.ino
+++ E96 Final Project.ino
@@ -1,13 +1,25 @@
 //Gloabl constants
 const unsigned short pinin = A0;
 
+//Global time variables
+unsigned long startMillis;
+unsigned long currentMillis;
+
 //Write a function to learn the 
 void setup() {
   Serial.begin(9600);
+  startMillis = millis();
 }
 
 void loop() {
+  //Update values
   int inputval = analogRead(pinin);
-  Serial.println(inputval);
+  currentMillis = millis() - startMillis;
+
+  //Print values to Serial monitor
+  Serial.print(inputval);
+  Serial.print(',');
+  Serial.print(currentMillis);
+  Serial.println();
   delay(15);
 }
