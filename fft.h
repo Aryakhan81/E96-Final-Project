5b7d830da052ffad8190062469e89df360c2a1be
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2022-12-02 10:15:29 -0800

    Add new FFT (kinda works ig)

--- fft.h
+++ fft.h
@@ -1,4 +1,8 @@
-float sine(int i);
-float cosine(int i);
-float* FFT(int in[],int N,float Frequency);
+// float sine(int i);
+// float cosine(int i);
+// float* FFT(int in[],int N,float Frequency);
 
+int fast_sine(int Amp, int th);
+int fast_cosine(int Amp, int th);
+int fastRSS(int a, int b);
+float Approx_FFT(int in[],int N,float Frequency);
