c22a2b83800ec12e6c1d462c0e72ae4681255b64
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2022-12-02 20:55:10 -0800

    Delete fft.h

--- E96 Final Project/fft.h
+++
@@ -1,8 +0,0 @@
-// float sine(int i);
-// float cosine(int i);
-// float* FFT(int in[],int N,float Frequency);
-
-int fast_sine(int Amp, int th);
-int fast_cosine(int Amp, int th);
-int fastRSS(int a, int b);
-float Approx_FFT(int in[],int N,float Frequency);
